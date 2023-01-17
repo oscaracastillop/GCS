1f5507bfdf12595108ab7fe92a79570244393d49
oscaracastillop <39472380+oscaracastillop@users.noreply.github.com>
2023-01-17 11:46:41 -0500

    Login

---
+++ Gcs.App/JsApp/Login.js
@@ -0,0 +1,27 @@
+﻿function Ingresar() {
+    let Usuario = $('#InputUsuario').val()
+    let Password = $('#InputPassword').val()
+    if (Usuario == '' || Usuario == null || Usuario == undefined) {
+        Swal.fire('Mensaje del Sistema', 'Por favor ingrese el nombre de Usuario', 'info');
+    }
+    else if (Password == '' || Password == null || Password == undefined) {
+        Swal.fire('Mensaje del Sistema', 'Por favor ingrese la contraseña', 'info');
+    }
+    else {
+        $.ajax({
+            type: 'POST',
+            dataType: 'Json',
+            url: '/Login/IniciarSesion',
+            data: { Usuario: Usuario, Password: Password },
+            success: function (resultado) {
+                valor = resultado.split('*');
+                if (valor[0] == 'OK') {
+                    Cookies.set('IdUser', valor[1]);
+                    window.location.href = '/Home/Inicio';
+                } else {
+                    Swal.fire('Mensaje del Sistema', valor[1], 'info');
+                }
+            },
+        });
+    }
+}
