58557174ad78b412321424c8e13c6e25b90217da
oscaracastillop <39472380+oscaracastillop@users.noreply.github.com>
2023-01-18 16:25:37 -0500

    Menu Home

--- Gcs.App/JsApp/Menu.js
+++ Gcs.App/JsApp/Menu.js
@@ -8,9 +8,9 @@
         success: function (resultado) {
             var contador = 0;
             $.each(resultado, function () {
-                $("#MenuBotones").append('<a href="' + resultado[contador].Ruta + '" class="btn" style="margin-left:10px;margin-right:10px;margin-top: 10px;margin-bottom:10px; height:100px; width:120px;border-radius:10%;" id = "' + resultado[contador].Id + '">' +
+                $("#MenuBotones").append('<a href="' + resultado[contador].Ruta + '" class="btn btnMenu" id = "' + resultado[contador].Id + '">' +
                     '' + resultado[contador].Icono + '<br>' +
-                    '<span style="font-size: 12px; color:black; font-weight:bold"> ' + resultado[contador].Nombre + '</span>' +
+                    '<span class="spanMenu"> ' + resultado[contador].Nombre + '</span>' +
                     '</a>'
                 );
                 contador++;
