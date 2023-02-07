c1330107c42d2eab1529b2f2d1ebe968f218f209
oscaracastillop <39472380+oscaracastillop@users.noreply.github.com>
2023-02-07 14:34:57 -0500

    fix controller modulo

---
+++ Gcs.App/Controllers/ModuloController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gcs.App.Controllers
+{
+    public class ModuloController : Controller
+    {
+        // GET: Modulo
+
+        public ActionResult Empresa()
+        {
+            return View();
+        }
+
+        public ActionResult Empleado()
+        {
+            return View();
+        }
+
+        public ActionResult Contratista()
+        {
+            return View();
+        }
+        public ActionResult Contabilidad()
+        {
+            return View();
+        }
+        public ActionResult Cliente()
+        {
+            return View();
+        }
+        public ActionResult Almacen()
+        {
+            return View();
+        }
+        public ActionResult Mantenimiento()
+        {
+            return View();
+        }
+        public ActionResult Configuracion()
+        {
+            return View();
+        }
+        public ActionResult Administracion()
+        {
+            return View();
+        }
+        public ActionResult Usuario()
+        {
+            return View();
+        }
+        public ActionResult Informacion()
+        {
+            return View();
+        }
+    }
+}
